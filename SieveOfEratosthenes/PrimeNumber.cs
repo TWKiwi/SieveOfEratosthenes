a7ceb093ad374ee9f683817d7b5418ed680324f4
maxwell.chen <maxwell.chen@titansoft.com.sg>
2019-03-03 17:09:55 +0800

    InitData Failed

---
+++ SieveOfEratosthenes/PrimeNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SieveOfEratosthenes
+{
+    public class PrimeNumber
+    {
+        public PrimeNumber(int num)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> InitNumbers { get; set; }
+    }
+}
---
+++ SieveOfEratosthenes/PrimeNumberTesting.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SieveOfEratosthenes
+{
+    [TestClass]
+    public class PrimeNumberTesting
+    {
+        [TestMethod]
+        public void InitWithNumAndShouldGetRangeOfNumbers()
+        {
+            var primeNumber = new PrimeNumber(120);
+
+            var initNumbers = primeNumber.InitNumbers;
+
+            CollectionAssert.AreEqual(Enumerable.Range(1, 120).ToList(), initNumbers.ToList());
+        }
+    }
+}
