b3eb7e9b2770e4d4f64148910825f6c758dc0c92
maxwell.chen <maxwell.chen@titansoft.com.sg>
2019-03-03 17:21:37 +0800

    inti data pass

--- SieveOfEratosthenes/PrimeNumber.cs
+++ SieveOfEratosthenes/PrimeNumber.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SieveOfEratosthenes
 {
     public class PrimeNumber
     {
+        public List<int> InitNumbers { get; set; }
+
         public PrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            InitNumbers = Enumerable.Range(1,num).ToList();
         }
-
-        public List<int> InitNumbers { get; set; }
     }
 }
